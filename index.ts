9d0de1b8ec298fea5bec0df7e2c8dcc031c172e2
ERUM WARIS <159144847+erum-waris@users.noreply.github.com>
2024-04-01 21:01:42 +0500

    created todo_list_app

---
+++ index.ts
@@ -0,0 +1,52 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+ console.log("****WELCOME TO MY TODO APP****");
+
+let todos: string[] = [];
+
+let loop :boolean = true ;
+
+
+while( loop ){
+
+  const addTask :{
+
+    todo:"string",
+
+    addmore :boolean,
+
+  }
+  
+= await inquirer.prompt([
+    {
+
+  name: "todo",
+  type: "input",
+  message: "what you wanna add in your todo_list?",
+    
+},
+
+{
+    name:"addMore",
+    type:"confirm",
+    message:"Are you sure to add item in your todo_list?",
+    default:"false",
+},
+]);
+todos.push(addTask.todo);
+
+console.log(todos);}
+
+// //  conditions addition if else statement
+// const  {todo , addmore }= addTask ;
+// loop = addmore;
+
+// if(todo)
+// {
+// todos.push(todo);
+// console.log("let's proceed")
+// }else{
+//   console.log("Please add something to make your todo list.")
+// }
+
